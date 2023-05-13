bcd561f9ea33dd8b9f8b340a9662cb7081dcc9ae
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-05-13 13:23:57 +0700

    1-pwa-session

---
+++ 0-project/notes-app-starter-project/src/js/index.js
@@ -0,0 +1,20 @@
+// Import our custom CSS
+import '../scss/main.scss';
+
+import App from './views/app';
+import * as bootstrap from 'bootstrap';
+
+const app = new App({
+    content: document.getElementById('mainContent'),
+    navListContainer: document.querySelectorAll(
+        '#navListContainer, #navListOffcanvas'
+    ),
+});
+
+window.addEventListener('hashchange', () => {
+    app.renderPage();
+});
+
+window.addEventListener('load', async () => {
+    app.renderPage();
+});
---
+++ 1-pwa/materi.md
@@ -0,0 +1,18 @@
+# Komponen Pembentuk PWA
+
+## Inti
+
+1. Application Shell
+
+-   Base UI yang bersifat static. contoh: Navbar, Drawer, dsb.
+-   Base UI disimpan dalam cache agar lebih cepat ditampilkan
+
+2. Web App Manifest
+3. Service Worker
+
+## Pendukung
+
+1. Cache API
+2. Fetch API
+3. IndexDB
+4. dsb
